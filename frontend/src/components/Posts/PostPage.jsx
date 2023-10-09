ea09206fd5dd4c6a6e476044ecd001b64e95e752
OGALEXI <64463918+OGALEXI@users.noreply.github.com>
2023-10-08 20:21:07 -0600

    post page and add comment

--- frontend/src/components/Posts/PostPage.jsx
+++ frontend/src/components/Posts/PostPage.jsx
@@ -1,20 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from 'react-router-dom';
 import { loadPostDetails } from "../../store/posts";
 import { getAllUsers } from "../../store/session";
 import './PostPage.css'
 import likeHeart from '../../assets/empty_heart.png';
-import PostComments from "./PostComments";
+import Comment from "./Comments";
+import { fetchPostComments, createComment } from "../../store/comments";
 
 function PostPage() {
     const dispatch = useDispatch();
     const { postId } = useParams();
     const post = useSelector((state) => state?.posts[postId]);
     const user = useSelector((state) => state?.session.users[post?.user_id])
+    const postComments = useSelector((state) => state.comments[postId]);
+    const [comment, setComment] = useState('');
+
+    console.log(postId)
+
+    const addComment = async (e) => {
+        e.preventDefault();
+        const data = await dispatch(
+            createComment(comment, postId)
+        );
+        if (data.errors) {
+            return console.log('An error has occurred: ', data.errors)
+        } else {
+            await dispatch(fetchPostComments(postId));
+            return setComment('')
+        }
+    }
 
     useEffect(() => {
         dispatch(loadPostDetails(postId));
+         dispatch(fetchPostComments(postId))
         dispatch(getAllUsers());
     }, [dispatch, postId])
 
@@ -37,7 +56,21 @@ function PostPage() {
                         </div>
                     </div>
                     <aside id="pp-comments-container">
-                        <PostComments postId={postId}/>
+                        {postComments?.map((comment) => (
+                            <div key={comment.id}>
+                                <Comment comment={comment} />
+                            </div>
+                        ))}
+                        <form onSubmit={addComment} id='add-comment-form'>
+                            <input
+                                type="text"
+                                value={comment}
+                                onChange={(e) => setComment(e.target.value)}
+                                required
+                                placeholder='Add a comment...'
+                            />
+                            <button type="submit">Post</button>
+                        </form>
                     </aside>
                     </section>
                 </div>
--- frontend/src/store/comments.js
+++ frontend/src/store/comments.js
@@ -1,9 +1,35 @@
 const SET_POST_COMMENTS = 'comments/SET_POST_COMMENTS';
+const ADD_COMMENT = '/comments/ADD_COMMENT';
 
 const setComments = (comments) => ({
   type: SET_POST_COMMENTS,
   payload: comments,
 });
+
+const addCommentAction = (comment) => ({
+  type: ADD_COMMENT,
+  payload: comment,
+});
+
+export const createComment = (content, postId) => async (dispatch) => {
+  try {
+    const res = await fetch(`/comments/${postId}/new`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        content,
+      }),
+    });
+    if (res.ok) {
+      const newComment = await res.json();
+      dispatch(addCommentAction(newComment));
+      return newComment;
+    }
+  } catch (error) {
+    console.log('Error', error);
+    return error;
+  }
+};
 
 export const fetchPostComments = (postId) => async (dispatch) => {
   try {
@@ -26,8 +52,18 @@ export default function commentReducer(state = {}, action) {
   let newState = { ...state };
   switch (action.type) {
     case SET_POST_COMMENTS:
-      const postComments = action.payload;
-      newState.post_comments = postComments;
+      const postComments = action.payload.comments;
+      if (postComments.length) {
+        newState[postComments[0].post_id] = postComments;
+      }
+      return newState;
+    case ADD_COMMENT:
+      const newComment = action.payload;
+      const postId = newComment.post_id;
+      if (!newState[postId]) {
+        newState[postId] = [];
+      }
+      newState[postId].push(newComment);
       return newState;
     default:
       return state;
